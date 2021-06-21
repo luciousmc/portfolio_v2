9d52facaea0d4b23f91d59824bcc606b68812e31
Marlon Clay <luciousmc@icloud.com>
2021-06-21 11:23:46 -0700

    add jsconfig

---
+++ components/Header.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+
+function Header() {
+  return (
+    <header className='fixed h-20 w-full flex items-center justify-between px-10 py-5 shadow-lg'>
+      <div className='flex-grow'>
+        {/* Title */}
+        <h1 className='font-medium'>Brotahman</h1>
+      </div>
+      <div>
+        <nav>
+          <ul className='flex mr-5 space-x-8'>
+            <li>
+              <Link href="/">
+                <a className='navLink'>&lt;Home /&gt;</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#About">
+                <a className='navLink'>&lt;About /&gt;</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#Projects">
+                <a className='navLink'>&lt;Projects /&gt;</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#Contact">
+                <a className='navLink'>&lt;Contact /&gt;</a>
+              </Link>
+            </li>            
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <Link href='#'>
+          <a className='text-neon border-2 py-2 px-3 border-neon rounded-md transition-colors duration-300 hover:bg-neon-tint'>
+            Resume
+          </a>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          DEFAULT: '#0a192f',
+          light: '#112240',
+          lightest: '#233554'
+        },
+        slate: {
+          DEFAULT: '#8892b0',
+          light: '#a8b2d1',
+          lightest: '#ccd6f6'
+        },
+        neon: {
+          DEFAULT:  '#64ffda',
+          tint: 'rgba(100,255,218,0.1)'
+        },
+        white: '#e6f1ff',
+        base: '#313541',
+        accent: '#ff9757'
+      },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
