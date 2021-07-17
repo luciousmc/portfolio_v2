fb1c714d142b40972d6401b2adf849f0c6af72a8
Marlon Clay <luciousmc@icloud.com>
2021-07-16 20:13:00 -0700

    social bar links updated

--- components/SocialBox.js
+++ components/SocialBox.js
@@ -1,11 +1,16 @@
-import { Facebook, Linkedin, Instagram, Twitter } from '@icons-pack/react-simple-icons'
+import { Facebook, Linkedin, Instagram, Twitter } from '@icons-pack/react-simple-icons';
+import { MailIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 
 function SocialBox() {
   return (
     <aside className='hidden md:flex flex-col fixed left-20 bottom-0'>
-      <Facebook className='social-icon' />
+      <Link href="https://www.facebook.com/marlon.lucious/">
+        <Facebook className='social-icon' />
+      </Link>
       <Linkedin className='social-icon' />
       <Twitter className='social-icon' />
+      <MailIcon className='social-icon' />
     </aside>
   )
 }
