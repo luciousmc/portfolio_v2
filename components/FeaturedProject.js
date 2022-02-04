476bf88d8cb6d7b47fc98e86f6a682e7097466ed
Marlon Clay <luciousmc@icloud.com>
2022-02-03 20:45:47 -0800

    feat: finish mobile style for featured component

--- components/FeaturedProject.js
+++ components/FeaturedProject.js
@@ -1,32 +1,52 @@
 import Link from 'next/dist/client/link';
-function FeaturedProject() {
-  return (
-    <div className='relative max-w-600px shadow-md'>
-      <div className='absolute opacity-[4%] top-0 left-0 right-0 bottom-0 z-[-1]'>
-        <img
-          className='w-full h-full object-cover object-left-top'
-          src='/images/projects/amazon_preview.png'
-          alt='Amazon Photo'
-        />
-      </div>
+import { Github } from '@icons-pack/react-simple-icons';
+import { ExternalLinkIcon } from '@heroicons/react/solid';
+import projects from '../details/project_info';
 
-      <div className='px-10 py-20 z-50'>
-        <h2 className='text-accent'>Featured Project</h2>
-        <h3 className='text-3xl tracking-wider hover:text-neon'>
-          <Link href='http://www.Amazon.com'>Amazon Clone</Link>
-        </h3>
-        <p className='mt-7 mb-4 text-xl font-light'>
-          A copy of the popular Amazon website. It features the ability to add
-          items to the cart and purchase them.
-        </p>
-        <ul className='flex gap-5'>
-          <li>React</li>
-          <li>TailwindCSS</li>
-          <li>NextJs</li>
-          <li>Stripe CLI</li>
-        </ul>
+function FeaturedProject() {
+  return projects.map(
+    ({
+      name,
+      description,
+      repo_link,
+      live_link,
+      featured,
+      project_img,
+      tech,
+    }) => (
+      <div className='relative max-w-600px shadow-md mt-5 mb-12'>
+        <div className='absolute opacity-[4%] top-0 left-0 right-0 bottom-0 z-[-1]'>
+          <img
+            className='w-full h-full object-left-top'
+            src={project_img}
+            alt={`${name} Image`}
+          />
+        </div>
+
+        <div className='px-10 py-20 z-50'>
+          <h2 className='text-accent'>Featured Project</h2>
+          <h3 className='text-3xl tracking-wider hover:text-neon'>
+            <Link href={live_link}>{name}</Link>
+          </h3>
+          <p className='mt-7 mb-4 text-xl font-light'>{description}</p>
+          <ul className='flex gap-4'>
+            {tech.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+
+          <div className='flex items-center space-x-3 mt-8'>
+            <Link href={repo_link}>
+              <Github className='w-10 navLink' />
+            </Link>
+
+            <Link href={live_link}>
+              <ExternalLinkIcon className='w-8 navLink' />
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
--- details/project_info.js
+++ details/project_info.js
@@ -1,26 +1,22 @@
-export const projectInfo = [
+export default [
   {
     name: 'Amazon Clone',
     description:
       'A copy of the popular Amazon website. It features the ability to add items to the cart and purchase them.',
-    tech_used: [
-      'React',
-      'TailwindCSS',
-      'NextJs',
-      'Stripe Payments',
-      'Stripe CLi',
-    ],
+    tech: ['React', 'TailwindCSS', 'NextJs', 'Stripe Payments', 'Stripe CLi'],
     featured: true,
     live_link: 'https://r-amazon.marlonclay.com',
-    github_link: 'https://github.com/luciousmc/r-amazon',
+    repo_link: 'https://github.com/luciousmc/r-amazon',
+    project_img: '/images/projects/amazon_preview.png',
   },
   {
     name: 'My Hero Memory',
     description:
       'A memory match game modeled after the popular anime My Hero Academia.',
-    tech_used: ['HTML5', 'CSS3', 'JavaScript', 'jQuery'],
+    tech: ['HTML5', 'CSS3', 'JavaScript', 'jQuery'],
     featured: true,
     live_link: 'https://my-hero-memory.marlonclay.com',
-    github_link: 'https://github.com/luciousmc/my-hero-memory',
+    repo_link: 'https://github.com/luciousmc/my-hero-memory',
+    project_img: '/images/projects/my-hero-memory-preview.png',
   },
 ];
