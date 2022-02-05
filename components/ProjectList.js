a955d65f2a2bc810b34d3f6aa19f6b25b5c01edd
Marlon Clay <luciousmc@icloud.com>
2022-02-04 20:56:16 -0800

    feat: add external github/live links for project card

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -1,17 +1,26 @@
 import { ExternalLinkIcon } from '@heroicons/react/outline';
 import { Github } from '@icons-pack/react-simple-icons';
 import projects from '../details/projects';
-
+import { useRouter } from 'next/router';
 function ProjectList() {
+  const router = useRouter();
   return (
     <div className='flex gap-3 justify-center'>
       {projects.map(({ name, description, tech, live_link, repo_link }) => (
-        <div className='bg-gray-700 shadow-md p-5 rounded-md w-72'>
+        <div
+          onClick={() => router.push(live_link)}
+          className='bg-gray-700 shadow-md p-5 cursor-pointer rounded-md w-72 group hover:-translate-y-2 transition duration-200'
+        >
           <div className='flex gap-3 items-center justify-end'>
-            <Github className='w-7' />
-            <ExternalLinkIcon className='w-6' />
+            <a href={repo_link} className='navLink'>
+              <Github className='w-6' />
+            </a>
+
+            <a href={live_link} className='navLink'>
+              <ExternalLinkIcon className='w-6' />
+            </a>
           </div>
-          <h2 className='text-2xl my-2'>{name}</h2>
+          <h2 className='text-2xl my-2 group-hover:text-neon'>{name}</h2>
           <p className='py-3 font-light text-xl'>{description}</p>
           <ul className='flex text-xs gap-3 flex-wrap mt-3'>
             {tech.map((item) => (
