8896ed0f7249abc3a7cbc051c6031c554be64f7b
Marlon Clay <luciousmc@icloud.com>
2022-02-25 16:29:47 -0800

    feat: add transition for mobile menu
    Tailwind does not have width/height transition enabled by default. Modify tailwind config to enable.

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -6,30 +6,33 @@ function SideMenu() {
 
   return (
     <>
-      <button
-        onClick={() => setIsMenuOpen((isOpen) => !isOpen)}
-        className='absolute top-0 right-0 p-4 z-[100] md:hidden outline-none border-0'
-      >
-        <MenuIcon className='h-10 text-slate' />
-      </button>
-      <div
-        className={`${
-          !isMenuOpen
-            ? 'hidden'
-            : 'fixed z-50 w-full h-full bg-[rgba(0,0,0,.5)]'
-        }`}
-      >
-        <menu className='absolute right-0 pt-28 w-64 h-full flex flex-col items-end bg-base my-0'>
-          <nav className='text-center w-full'>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Projects</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        </menu>
+      <div className='fixed top-0 right-0 md:hidden z-[100]'>
+        <button
+          onClick={() => setIsMenuOpen((isOpen) => !isOpen)}
+          className='p-4 outline-none border-0'
+        >
+          <MenuIcon className='h-10 text-slate' />
+        </button>
       </div>
+      <div
+        className={`fixed ${
+          !isMenuOpen ? 'hidden' : 'z-50 w-full h-full bg-[rgba(0,0,0,.5)]'
+        }`}
+      ></div>
+      <menu
+        className={`fixed right-0 z-[60] ${
+          !isMenuOpen ? 'w-0 duration-100' : 'w-64'
+        } pt-28 h-full flex flex-col items-end bg-base my-0 transition-width duration-200 linear`}
+      >
+        <nav className='text-center w-full'>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </menu>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,28 @@ module.exports = {
         navy: {
           DEFAULT: '#0a192f',
           light: '#112240',
-          lightest: '#233554'
+          lightest: '#233554',
         },
         slate: {
           DEFAULT: '#8892b0',
           light: '#a8b2d1',
-          lightest: '#ccd6f6'
+          lightest: '#ccd6f6',
         },
         neon: {
-          DEFAULT:  '#64ffda',
-          tint: 'rgba(100,255,218,0.1)'
+          DEFAULT: '#64ffda',
+          tint: 'rgba(100,255,218,0.1)',
         },
         white: '#e6f1ff',
         base: '#313541',
-        accent: '#ff9757'
+        accent: '#ff9757',
       },
-    }
+      transitionProperty: {
+        width: 'width',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
