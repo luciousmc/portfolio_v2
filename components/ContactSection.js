62f31d03331865652c032a22f130fe9f7c03cb20
Marlon Clay <luciousmc@icloud.com>
2023-01-23 21:02:06 -0800

    feat: add form validation via react-hook-form

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -1,27 +1,76 @@
+import { useForm } from "react-hook-form";
+
 function ContactSection() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className='space-y-8 w-10/12 md:w-3/5 mx-auto bg-gray-700 p-5 md:p-16 rounded-md'>
       <h3 className='text-3xl text-center'>Let&apos;s Talk</h3>
-      <p className='text-center text-gray-400'>I&apos;d like to work with you to make your idea come to life. Drop me a line!</p>
-      <form className='flex flex-col space-y-5'>
+      <p className='text-center text-gray-400'>
+        I&apos;d like to work with you to make your idea come to life. Drop me a
+        line!
+      </p>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='flex flex-col space-y-5'
+      >
         <div className='flex flex-col'>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id='name' name='name' />
+          <label htmlFor='name'>Name:</label>
+          <input
+            {...register("name", {
+              required: "Name field is required",
+              maxLength: 15,
+            })}
+            type='text'
+          />
+          {errors.name && (
+            <p className='text-red-500' role='alert'>
+              {errors.name.message}
+            </p>
+          )}
         </div>
 
         <div className='flex flex-col'>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id='email' name='email' />
+          <label htmlFor='email'>Email:</label>
+          <input
+            {...register("email", {
+              required: "Email is required",
+              maxLength: 20,
+            })}
+            type='email'
+          />
+          {errors.email && (
+            <p className='text-red-500' role='alert'>
+              {errors.email.message}
+            </p>
+          )}
         </div>
 
         <div className='flex flex-col'>
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+          <label htmlFor='message'>Message</label>
+          <textarea
+            {...register("message", { required: "Message is required" })}
+            cols='30'
+            rows='10'
+          ></textarea>
+          {errors.message && (
+            <p className='text-red-500' role='alert'>
+              {errors.message.message}
+            </p>
+          )}
         </div>
 
-        <input type="submit" value="Send" className='cursor-pointer' />
+        <input type='submit' value='Send' className='cursor-pointer' />
       </form>
     </div>
-  )
+  );
 }
-export default ContactSection
+export default ContactSection;
