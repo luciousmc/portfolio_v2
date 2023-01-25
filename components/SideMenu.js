0b970db81489b7cc4512a8b4032fd16b5d1a1caf
Marlon Clay <luciousmc@icloud.com>
2023-01-25 15:44:31 -0800

    refactor: improve code fore side menu
    - Remove Unnecessary div
    ~ change properties for sidemenu animation from width to translate

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,6 +1,6 @@
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
-import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 function SideMenu() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -16,10 +16,10 @@ function SideMenu() {
         closeMenu();
       }
     };
-    overlayRef.current?.addEventListener('click', handleOverlayClick);
+    overlayRef.current?.addEventListener("click", handleOverlayClick);
 
     return () =>
-      overlayRef.current.removeEventListener('click', handleOverlayClick);
+      overlayRef.current.removeEventListener("click", handleOverlayClick);
   }, [isMenuOpen]);
 
   const handleNavLinkClick = () => {
@@ -28,31 +28,29 @@ function SideMenu() {
 
   return (
     <>
-      <div className='fixed top-0 right-0 md:hidden z-[100]'>
-        <button
-          onClick={() => setIsMenuOpen((isOpen) => !isOpen)}
-          className='p-4 outline-none border-0'
-        >
-          {isMenuOpen ? (
-            <XIcon className='text-neon' />
-          ) : (
-            <MenuIcon className='h-10 text-neon' />
-          )}
-        </button>
-      </div>
+      <button
+        onClick={() => setIsMenuOpen((isOpen) => !isOpen)}
+        className='fixed top-0 right-0 md:hidden z-[100] p-4 outline-none border-0'
+      >
+        {isMenuOpen ? (
+          <XIcon className='h-10 text-neon' />
+        ) : (
+          <MenuIcon className='h-10 text-neon' />
+        )}
+      </button>
       <div
         ref={overlayRef}
         className={`fixed ${
-          !isMenuOpen ? 'hidden' : 'z-50 w-full h-full bg-[rgba(0,0,0,.5)]'
+          !isMenuOpen ? "hidden" : "z-50 w-full h-full bg-[rgba(0,0,0,.5)]"
         }`}
       ></div>
       <menu
-        className={`fixed right-0 z-[60] ${
-          !isMenuOpen ? 'w-0 duration-100' : 'w-64'
+        className={`fixed w-64 z-[60] right-0 transform transition-transform ${
+          !isMenuOpen && "translate-x-full duration-100"
         } pt-28 h-full bg-base my-0 transition-width duration-200 linear`}
       >
         <nav className='text-center w-full h-full'>
-          <ul>
+          <ul className='space-y-3'>
             <li className=''>
               <Link href='#top'>
                 <a onClick={handleNavLinkClick} className='mobileNavLink'>
