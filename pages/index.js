30791832246e3915097a7e07e0ffd795fc1e8bcb
Marlon Clay <luciousmc@icloud.com>
2023-01-25 17:03:31 -0800

    chore: change site title

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import Head from 'next/head';
-import Header from '@components/Header';
-import HeroSection from '@components/HeroSection';
-import Section from '@components/Section';
-import SocialBox from '@components/SocialBox';
-import AboutSection from '@components/AboutSection';
-import SideMenu from '@components/SideMenu';
-import FeaturedProject from '@components/FeaturedProject';
-import ContactSection from '@components/ContactSection';
+import Head from "next/head";
+import Header from "@components/Header";
+import HeroSection from "@components/HeroSection";
+import Section from "@components/Section";
+import SocialBox from "@components/SocialBox";
+import AboutSection from "@components/AboutSection";
+import SideMenu from "@components/SideMenu";
+import FeaturedProject from "@components/FeaturedProject";
+import ContactSection from "@components/ContactSection";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Portfolio v2</title>
+        <title>Marlon Clay Portfolio</title>
         <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
