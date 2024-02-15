07cd089d3cae4ddfcb66478fad05d5781d251b2f
Marlon Clay <luciousmc@icloud.com>
2024-02-14 17:57:20 -0800

    chore: move socialbox to main container

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,11 @@ export default function Home() {
 
       <SideMenu />
 
-      <main className='max-w-7xl pb-14 md:px-8 mx-auto'>
+      <main className=' relative max-w-screen-2xl pb-14 md:px-8 mx-auto'>
+        <SocialBox />
+
         <div className='flex flex-col w-[95%] md:w-[85%] mx-auto'>
           <HeroSection />
-
-          <SocialBox />
 
           <Section id='About' title='About'>
             <AboutSection />
