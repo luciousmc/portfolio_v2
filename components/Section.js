96fe873c620949a24fa91ded04e4b3fd1685d6a4
Marlon Clay <luciousmc@icloud.com>
2024-02-14 17:56:52 -0800

    chore: reduce height between sections for larger screens

--- components/Section.js
+++ components/Section.js
@@ -1,6 +1,6 @@
 function Section({ id, title, children }) {
   return (
-    <section id={id} className='w-full min-h-screen pt-24'>
+    <section id={id} className='w-full min-h-[950px] pt-24'>
       <div className='flex items-center space-x-4 mt-5 mb-14'>
         <h2 className='flex-grow text-5xl font-medium'>{title}</h2>
         <div className='h-1 bg-neon w-full' />
@@ -8,7 +8,7 @@ function Section({ id, title, children }) {
 
       {children}
     </section>
-  )
+  );
 }
 
-export default Section
+export default Section;
